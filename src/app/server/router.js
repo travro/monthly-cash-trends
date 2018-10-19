036454540be490a5935d52dab0f36555c8ca9fac
travro <travro25@gmail.com>
2018-10-19 11:53:42 -0400

    changed query with call to stored procedure

--- src/app/server/router.js
+++ src/app/server/router.js
@@ -4,21 +4,7 @@ const database = require('./database');
 
 router.route('/')
     .get((req, res) => {
-        database.query(`
-        select 
-    transactions.id, 
-    transactions.date, 
-    transactions.amount,
-    vendors.vendor,
-    category.category
-    from transactions
-    inner join vendors 
-    	on transactions.vendors_id = vendors.id
-    inner join vendors_has_category 
-    	on vendors.id = vendors_has_category.vendors_id
-    inner join category
-    	on vendors_has_category.category_id = category.id
-        `, (err, results, fields) => {
+        database.query('CALL GetAllTransactions();',(err, results, fields) => {
                 if (err) console.log('Query error: ' + err);
                 res.send(results);
             })
