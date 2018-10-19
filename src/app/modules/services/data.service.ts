f5ac2ac3478af5daf343f06ef57edc3ef271d4ff
travro <travro25@gmail.com>
2018-10-19 18:06:24 -0400

    ..

--- src/app/modules/services/data.service.ts
+++ src/app/modules/services/data.service.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'
-
 //
 import { Transaction } from '../models/transaction.model';
-
 
 @Injectable({
   providedIn: 'root'
 })
-
-
 
 export class DataService {
   //https://5b82c6892fd7f2001417916a.mockapi.io/mock
@@ -21,7 +17,6 @@ export class DataService {
 
   getAllTransactions(): Observable<Transaction[]> {
     return this.http.get<Transaction[]>(this.baseUrl);
-
   }
 
 }
