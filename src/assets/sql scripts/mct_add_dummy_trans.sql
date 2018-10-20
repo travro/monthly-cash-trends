6bc8ab5e87b9fd645654ca32d2487303cf9cfe44
travro <travro25@gmail.com>
2018-10-20 19:45:14 -0400

    added sql scripts

---
+++ src/assets/sql scripts/mct_add_dummy_trans.sql
@@ -0,0 +1,12 @@
+use mct;
+
+insert into transactions (dt, vendor, amount) values 
+(curdate(), 'DummyVendor1', 3.45),
+(curdate(), 'DummyVendor2', 11.47),
+(curdate(), 'DummyVendor3', 73.22),
+(curdate(), 'DummyVendor4', 199.27),
+(curdate(), 'DummyVendor5', 34.56),
+(curdate(), 'DummyVendor6', 18.99),
+(curdate(), 'DummyVendor7', 1.45),
+(curdate(), 'DummyVendor8', 2.41)
+;
---
+++ src/assets/sql scripts/mct_create_all.sql
@@ -0,0 +1,38 @@
+create database mct;
+use mct;
+
+create table if not exists categories(
+id int auto_increment not null,
+category varchar(255),
+primary key (id)
+);
+
+create table if not exists transactions(
+id int auto_increment not null,
+dt date,
+vendor varchar(255),
+amount decimal(7,2),
+cat_id int default 1,
+primary key(id),
+foreign key(cat_id) references categories(id)
+);
+
+create table if not exists budgets_2018(
+id int not null,
+January decimal(7,2) default 0.00,
+February decimal(7,2) default 0.00,
+March decimal(7,2) default 0.00,
+April decimal(7,2) default 0.00,
+May decimal(7,2) default 0.00,
+June decimal(7,2) default 0.00,
+July decimal(7,2) default 0.00,
+August decimal(7,2) default 0.00,
+September decimal(7,2) default 0.00,
+October decimal(7,2) default 0.00,
+November decimal(7,2) default 0.00,
+December decimal(7,2) default 0.00,
+primary key(id),
+foreign key(id) references categories(id)
+);
+insert into categories (category) values ('Uncategorized');
+insert into budgets_2018(id) values ((select id from categories where category = 'Uncategorized'));
