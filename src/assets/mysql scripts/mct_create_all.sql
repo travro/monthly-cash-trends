b147bdde6faaf96e10f3961b63abb67700f109c7
travro <travro25@gmail.com>
2019-03-03 18:52:30 -0500

    CHANGE mct_create_all script
    Changed script to add agg/max budget tables, dropped original

--- src/assets/mysql scripts/mct_create_all.sql
+++ src/assets/mysql scripts/mct_create_all.sql
@@ -17,7 +17,25 @@ primary key(id),
 foreign key(cat_id) references categories(id)
 );
 
-create table if not exists budgets_2018(
+create table if not exists b19_agg(
+id int not null,
+January decimal(7,2) default 0.00,
+February decimal(7,2) default 0.00,
+March decimal(7,2) default 0.00,
+April decimal(7,2) default 0.00,
+May decimal(7,2) default 0.00,
+June decimal(7,2) default 0.00,
+July decimal(7,2) default 0.00,
+August decimal(7,2) default 0.00,
+September decimal(7,2) default 0.00,
+October decimal(7,2) default 0.00,
+November decimal(7,2) default 0.00,
+December decimal(7,2) default 0.00,
+primary key(id),
+foreign key(id) references categories(id)
+);
+
+create table if not exists b19_max(
 id int not null,
 January decimal(7,2) default 0.00,
 February decimal(7,2) default 0.00,
@@ -35,4 +53,5 @@ primary key(id),
 foreign key(id) references categories(id)
 );
 insert into categories (category) values ('Uncategorized');
-insert into budgets_2018(id) values ((select id from categories where category = 'Uncategorized'));
+insert into b19_agg(id) values ((select id from categories where category = 'Uncategorized'));
+insert into b19_max(id) values ((select id from categories where category = 'Uncategorized'));
