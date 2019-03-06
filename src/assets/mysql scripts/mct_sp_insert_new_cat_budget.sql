cb378192c135add58432c8f109648f72cabc0296
TravisWork <travis.melvin@sumtotalsystems.com>
2019-03-06 17:30:42 -0500

    DELETE mct_sp_insert_new_cat_bud
    replaced file

--- src/assets/mysql scripts/mct_sp_insert_new_cat_budget.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROCEDURE `insert_new_category_budget` (in c varchar(50))
-BEGIN
-insert into categories(category) values (c);
-
-set @newId = (select id from categories where categories.category = c);
-insert into budgets_20180(id) values (newId);
-END
